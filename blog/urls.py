54648a2e2e9c17f2686016e19c9e291520900f37
czr <303170942@qq.com>
2018-12-16 21:12:38 +0800

    Revert "Revert "使用haystack， whoosh， jieba完善搜索功能，在文章内容中显示关键词高亮""
    This reverts commit 10a15fd7a6bf2ee0c4f96deb6d3c1b0a213cb9d0.

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('archives/<int:year>/<int:month>', views.ArchivesView.as_view(), name='archives'),
     path('category/<int:id>', views.CategoryView.as_view(), name='category'),
     path('tag/<int:id>', views.TagView.as_view(), name='tag'),
+    # path('search/', views.search, name='search'),
 ]
